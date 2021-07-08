9e3e2c7e54e84199992f3afb500cc88743396b45
Possible-99 <juanpsfi3023@gmail.com>
2021-07-08 13:51:10 -0500

    Adding another UI

--- frontend/src/components/fileDragger/fileDragger.component.jsx
+++ frontend/src/components/fileDragger/fileDragger.component.jsx
@@ -1,22 +1,77 @@
 import React from 'react'
-import { InboxOutlined } from '@ant-design/icons';
-import {Upload} from "antd";
-import "./fileDragger.styles.scss";
-const { Dragger } = Upload;
+import {Upload,Button, message } from "antd";
+import { UploadOutlined } from '@ant-design/icons'
+import axios from 'axios';
+import "./fileDragger.styles.scss"
 
-const FileDragger=(props)=>{
+const FileDragger=({state,setState})=>{
+
+    const handleUpload = () => {
+        const { fileList } =state;
+        const formData = new FormData();
+        fileList.forEach(file => {
+          formData.append('file', file);
+        });
+    
+        setState({...state,
+          uploading: true,
+        });
+    
+        // You can use any AJAX library you like
+        axios.post('/api/priori', {formData,inputValues:state.inputsStatus}, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+        }).then(()=>{setState({
+            ...state,
+            uploading: false,
+          })
+          message.success('upload successfully.')})
+          .catch(()=>{
+            setState({
+              uploading: false,
+            });
+            message.error('upload failed.');
+          })
+      };
+    
+      const {fileList,uploading } = state;
+      const props = {
+          accept:".csv",
+        onRemove: file => {
+          setState(state => {
+            const index = state.fileList.indexOf(file);
+            const newFileList = state.fileList.slice();
+            newFileList.splice(index, 1);
+            return {...state,
+              fileList: newFileList,
+            };
+          });
+        },
+        beforeUpload: file => {
+          setState(state => ({...state,
+            fileList: [...state.fileList, file],
+          }));
+          return false;
+        },
+        fileList,
+      };
 
     return(
-        <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-            <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        <p className="ant-upload-hint">
-            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-            band files
-        </p>
-        </Dragger>
+    <div>
+        <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Selecciona o arrastra el archivo</Button>
+      </Upload>
+      <Button
+        type="primary"
+        onClick={handleUpload}
+        disabled={fileList.length === 0 || Object.values(state.inputsStatus).indexOf(null) > -1 }
+        loading={uploading}
+        style={{ marginTop: 16 }}
+      >
+        {uploading ? 'Uploading' : 'Start Upload'}
+      </Button>
+      </div>
     )
 }
 
--- frontend/src/pages/priori/priori.component.jsx
+++ frontend/src/pages/priori/priori.component.jsx
@@ -3,37 +3,18 @@ import "./priori.styles.scss";
 import MyLayout from "../../components/layout/layout.component";
 import { Layout,Row,Col,InputNumber,Divider,message} from "antd";
 import FileDragger from "../../components/fileDragger/fileDragger.component";
-
 const { Content } = Layout;
 
 const Priori = () => {
-  const [state, setState] = useState({support:null,confidence:null,lift:null,minElements:null})
-  const props = {
-    name: 'file',
-    accept:".csv",
-    data:state,
-    maxCount:"1",
-    disabled:Object.values(state).indexOf(null) > -1?true:false,
-    multiple: false,
-    action: '/api/priori',
-    onChange(info) {
-      const { status } = info.file;
-      if (status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`);
-        console.log(info.response)
-      } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-    onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
-    },
-  };
+  const [state, setState] = useState({
+    fileList: [],
+    uploading: false,
+    inputsStatus:{support:null,confidence:null,lift:null,minElements:null}
+  });
 
-  console.log(state)
+
+
+  
   return (
     <MyLayout menuKey="2" title="A priori">
       <Content style={{ margin: "5px 16px 0px 16px" }}>
@@ -45,19 +26,19 @@ const Priori = () => {
           <Row>
             <Col span={6}>
             <h3>Soporte</h3>
-            <InputNumber min={0.0001} max={100} onChange={e=>setState({...state,support:e})} /></Col>
+            <InputNumber min={0.0001} max={100} onChange={e=>setState({...state,inputsStatus:{...state.inputsStatus,support:e}})}/></Col>
             <Col span={6}>
             <h3>Confianza</h3>
-            <InputNumber min={0.00001} max={100} onChange={e=>setState({...state,confidence:e})} /></Col>
+            <InputNumber min={0.00001} max={100} onChange={e=>setState({...state,inputsStatus:{...state.inputsStatus,confidence:e}})}/></Col>
             <Col span={6}>
             <h3>Lift</h3>
-            <InputNumber min={0.00001} max={100} onChange={e=>setState({...state,lift:e})} /></Col>
+            <InputNumber min={0.00001} max={100} onChange={e=>setState({...state,inputsStatus:{...state.inputsStatus,lift:e}})} /></Col>
             <Col span={6}>
             <h3>Núm. de elementos min.</h3>
-            <InputNumber min={1} max={100} onChange={e=>setState({...state,minElements:e})}/></Col>
+            <InputNumber min={1} max={100} onChange={e=>setState({...state,inputsStatus:{...state.inputsStatus,minElements:e}})}/></Col>
           </Row>
           <Divider>Para subir el archivo primero completa los campos anteriores</Divider>
-          <FileDragger {...props} />
+          <FileDragger  state={state} setState={setState} />
         </div>
       </Content>
     </MyLayout>
